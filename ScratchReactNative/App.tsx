7852f80d4927946c6416ad6194b4fad060ef0783
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-12-05 21:13:42 +0800

    added TabBar

--- ScratchReactNative/App.tsx
+++ ScratchReactNative/App.tsx
@@ -8,30 +8,34 @@
 import React from 'react';
 import {Text, StyleSheet, Button} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import Screen from './app/screens/Screen';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import colors from './app/config/colors';
+
 
 // const Link = () => {
 //   const navigation = useNavigation();
 //   return(
-//   <Button 
-//     title='Click' 
+//   <Button
+//     title='Click'
 //     onPress={() => navigation.navigate('TweetDetails') }/>
 
 // )
 // }
 
-
 const Tweets = ({navigation}) => (
   <Screen style={styles.container}>
     <Text>Tweets</Text>
-    <Button 
-      title='View Tweet'
-      onPress={() => navigation.navigate('TweetDetails', {id:1})}/>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate('TweetDetails', {id: 1})}
+    />
   </Screen>
 );
 
-const TweetDetails = ({ route }) => (
+const TweetDetails = ({route}) => (
   <Screen style={styles.container}>
     <Text>Tweet Details {route.params.id}</Text>
   </Screen>
@@ -39,30 +43,55 @@ const TweetDetails = ({ route }) => (
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator 
+  <Stack.Navigator
     screenOptions={{
-      headerStyle: { backgroundColor: 'dodgerblue'},
+      headerStyle: {backgroundColor: 'dodgerblue'},
       headerTintColor: 'white',
     }}>
-    <Stack.Screen 
-      name="Tweets" 
-      component={Tweets} 
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
       options={{
-        headerStyle: { backgroundColor: 'tomato'},
+        headerStyle: {backgroundColor: 'tomato'},
         headerTintColor: 'white',
-      }}/>
-    <Stack.Screen 
-      name="TweetDetails" 
-      component={TweetDetails} 
-      options={({route}) => ({ title: route.params.id })}
+      }}
+    />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({route}) => ({title: route.params.id})}
     />
   </Stack.Navigator>
 );
 
+const Account = () => 
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveBackgroundColor: 'tomato',
+      tabBarActiveTintColor: 'white',
+      tabBarInactiveBackgroundColor: '#eee',
+      tabBarInactiveTintColor: 'black'
+    }}>
+    <Tab.Screen 
+      name="Feed" 
+      component={Tweets}
+      options={{
+        tabBarIcon: ({size,color}) => <AntDesignIcon name="home" size={size} color={color}/>
+      }} />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+)
+
 function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
